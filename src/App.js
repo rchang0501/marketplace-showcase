b9be31629b1044baa3b752b80675b0f620a9c72d
Ryan Chang <ryanchang0501@gmail.com>
2022-04-19 18:13:36 -0400

    configured tailwind

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Market Place Showcase</h1>
+      <h1 className="text-3xl font-bold underline">Market Place Showcase!</h1>
     </div>
   );
 };
