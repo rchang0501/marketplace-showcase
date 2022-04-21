21d71970ed9248882e5a864c07e00d4253ec97d4
Ryan Chang <ryanchang0501@gmail.com>
2022-04-21 18:41:53 -0400

    added max screen widths to tailwind config

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -8,19 +8,18 @@ const SectionWrapper = ({ title, description, showBtn, mockupImg, banner }) => {
     <div
       className={`min-h-screen ${styles.section} ${styles.bgWhite} ${banner}`}
     >
-      <div className={`flex items-center ${styles.boxClass}`}>
+      <div className={`flex items-center ${styles.boxClass} w-11/12 sm:w-full minmd:w-3/4`}>
         <div className={`${styles.descDiv} fadeLeftMini`}>
           <h1 className={`${styles.h1Text}`}>{title}</h1>
-          <p className={`${styles.descriptionText}`} >{description}</p>
+          <p className={`${styles.descriptionText}`}>{description}</p>
           {showBtn && (
-              <Button 
-                assetUrl={assets.expo}
-                link="deployed marketplace app"
-              />
+            <Button assetUrl={assets.expo} link="deployed marketplace app" />
           )}
         </div>
+        <div className={`flex-1 ${styles.flexCenter} p-8 sm:px-0`}>
+          <img src={mockupImg} alt="mockup" className={styles.img} />
+        </div>
       </div>
-      {/* <img src={mockupImg} alt="mockup" /> */}
     </div>
   );
 };
